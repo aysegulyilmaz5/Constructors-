4a8e27e71f43a326c4ec557c97d3504f7702e74f
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:44:58 +0300

    constructor ex3

---
+++ Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Worker
+    {
+        public Worker(string name,string surname,string Id)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(surname);
+            Console.WriteLine(Id);
+        }
+    }
+}
---
+++ workprogram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1;
+            Console.Write("Name:");
+            name1 = Console.ReadLine();
+            string surname1;
+            Console.Write("Surname:");
+            surname1 = Console.ReadLine();
+            string Id;
+            Console.Write("Id:");
+            Id = Console.ReadLine();
+            Console.Read();
+
+        }
+    }
+}
