5ec73aad679d372fe69e57d8b50835525e20c18f
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:32:29 +0300

    Constructor ex2

---
+++ Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inf;
+            Console.Write("Name - Club:");
+            inf = Console.ReadLine();
+            Student student = new Student("Ayşegül - Yılmaz");
+            Console.Read();
+        }
+    }
+}
---
+++ Student1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Student
+    {
+        public Student(string information)
+        {
+            Console.WriteLine(information);
+        }
+    }
+}
