497dcb5db7501e4e3c508e6f0354cf229e9a30a3
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:27:32 +0300

    Constructor ex1

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Student
+    {
+        public Student()
+        {
+            Console.WriteLine("Name: Aysegul");
+            Console.WriteLine("Surname: Yilmaz");
+            Console.WriteLine("Job : Computer Engineer");
+        }
+    }
+}
