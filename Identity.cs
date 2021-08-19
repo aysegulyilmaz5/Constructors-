fd4530769d2d2c94fa800c87b4352eee3c20fd9c
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-08-20 02:58:51 +0300

    Constructor ex4

---
+++ Identity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+    class Identity
+    {
+        string name;
+        string surname;
+        string born;
+        int age;
+        char gender;
+
+        public string NAME
+        {
+            get { return name; }
+            set { name = value.ToLower(); }
+        }
+        public string SURNAME
+        {
+            get { return surname; }
+            set { surname = value.ToUpper(); }
+        }
+        public string BORN
+        {
+            get { return born;}
+            set { born = value; }
+
+        }
+        public int AGE
+        {
+            get { return age; }
+            set { age = Math.Abs(value); }
+        }
+
+        public char GENDER
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public Identity()
+        {
+            name = "";
+            surname = "";
+            age = 18;
+            gender = 'f';
+            born = "New York";
+
+        }
+    }
+}
---
+++ Identityprogram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Identity ıd = new Identity();
+            ıd.NAME = "Aysegul";
+            ıd.SURNAME = "Yilmaz";
+            Console.WriteLine(ıd.NAME);
+            Console.WriteLine(ıd.SURNAME);
+            Console.WriteLine(ıd.BORN);
+            Console.WriteLine(ıd.AGE);
+            Console.WriteLine(ıd.GENDER);
+            Console.Read();
+        }
+    }
+}
